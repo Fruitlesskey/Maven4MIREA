673452c6a769c4e0d725626fbd5fd73b6eaf9fd2
Albert Islamau <fruitlesskey2@mail.ru>
2024-05-09 22:32:48 +0300

    rename Percent

--- src/main/java/com/example/maven4mirea/BonusService.java
+++ src/main/java/com/example/maven4mirea/BonusService.java
@@ -3,8 +3,8 @@ package com.example.maven4mirea;
 public class BonusService {
   public long calculate(long amount, boolean registered)
   {
-    int percent = registered ? 3 : 1;
-    long bonus = amount * percent / 100;
+    int Percent = registered ? 3 : 1;
+    long bonus = amount * Percent / 100;
     long limit = 500;
     if (bonus > limit) {
       bonus = limit;
